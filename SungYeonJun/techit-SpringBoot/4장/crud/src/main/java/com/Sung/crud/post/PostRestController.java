5b0ea1761a5d1bc1bb144194e1d2a86e536c6e7f
성연준 <elwlahs60@gmail.com>
2023-05-16 18:56:21 +0900

    [ADD] Spring Boot Chapter 4~5

--- SungYeonJun/techit-SpringBoot/4장/crud/src/main/java/com/Sung/crud/post/PostRestController.java
+++ SungYeonJun/techit-SpringBoot/4장/crud/src/main/java/com/Sung/crud/post/PostRestController.java
@@ -1,56 +1,75 @@
 package com.Sung.crud.post;
 
-import org.apache.juli.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-@ResponseBody
-//@RequestMapping("post")
-public class PostController {
-    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+
+@RestController
+@RequestMapping("post")
+public class PostRestController {
+    private static final Logger logger = LoggerFactory.getLogger(PostRestController.class);
     private final PostService postService;
-    public PostController(
+
+    public  PostRestController(
             @Autowired PostService postService
     ){
         this.postService = postService;
     }
 
-    @PostMapping("create")
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     public void createPost(@RequestBody PostDto postDto){
         logger.info(postDto.toString());
         this.postService.createPost(postDto);
     }
-    @GetMapping("read-all")
+
+    // http://localhost:8080/post
+    // GET /post
+    @GetMapping()
     public List<PostDto> readPostAll(){
         logger.info("in read all");
         return this.postService.readPostAll();
     }
+    // http://localhost:8080/post
+    // GET /post/0/
+    // GET /post?id=0
 
-    @GetMapping("read-one")
-    public PostDto readPostOne(@RequestParam("id)")int id){
+    @GetMapping("{id}")
+    public PostDto readPost(@PathVariable("id")int id){
         logger.info("in read one");
         return this.postService.readPost(id);
     }
-
-    @PostMapping("update")
+    
+    // PUT /post/0/
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public void updatePost(
-            @RequestParam("id") int id,
-            @RequestBody PostDto postDto
-    ){
+            @PathVariable("id")int id,
+            @RequestBody PostDto postDto)
+    {
         logger.info("target id: " + id);
         logger.info("update content: " + postDto);
         this.postService.updatePost(id, postDto);
     }
-    @DeleteMapping("delete")
-    public void deletePost(@RequestParam("id") int id){
+
+    // DELETE /post/0/
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deletePost(@PathVariable("id")int id){
+
 
         this.postService.deletePost(id);
     }
+
+    // http://localhost:8080/post
+    // Post /post
+    // REQUEST_BODY
+
 }
+
