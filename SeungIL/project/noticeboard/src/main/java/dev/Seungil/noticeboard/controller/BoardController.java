31cb49491d228fd5d3e0e7ac536eab91ad8e135e
승일 <seoung1515@naver.com>
2023-07-11 16:22:39 +0900

    [ADD] Spring Boot Swagger

--- SeungIL/project/noticeboard/src/main/java/dev/Seungil/noticeboard/controller/BoardController.java
+++ SeungIL/project/noticeboard/src/main/java/dev/Seungil/noticeboard/controller/BoardController.java
@@ -2,6 +2,7 @@ package dev.Seungil.noticeboard.controller;
 
 import dev.Seungil.noticeboard.dto.BoardDto;
 import dev.Seungil.noticeboard.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class BoardController {
 
         this.boardService = boardService;
     }
+    @Operation(summary = "test", description = "Main api example")
     @GetMapping("/") // 메인화면
     public String list(Model model, @RequestParam(value = "page", defaultValue = "1")Integer pageNum){
         List<BoardDto> boardDtoList = boardService.getBoardList(pageNum);
@@ -27,6 +29,7 @@ public class BoardController {
 
         return "board/list.html";
     }
+    @Operation(summary = "test", description = "writing api example")
     @GetMapping("/post") // 글쓰기
     public String write(){
         return "board/write.html";
@@ -36,6 +39,7 @@ public class BoardController {
         boardService.savePost(boardDto);
         return "redirect:/";
     }
+    @Operation(summary = "test", description = "post 'detail' api example")
     @GetMapping("/post/{no}") // 쓴거 누르면 detail하게 보여줌
     public String detail(@PathVariable("no") Long id, Model model){
         BoardDto boardDto = boardService.getPost(id);
@@ -43,6 +47,7 @@ public class BoardController {
         model.addAttribute("boardDto", boardDto);
         return "board/detail.html";
     }
+    @Operation(summary = "test", description = "edit api example")
     @GetMapping("/post/edit/{no}")
     public String edit(@PathVariable("no") Long id, Model model){
         BoardDto boardDto = boardService.getPost(id);
@@ -51,18 +56,21 @@ public class BoardController {
         return "board/update.html";
     }
 
+    @Operation(summary = "test", description = "update api example")
     @PutMapping("/post/edit/{no}") // 수정
     public String update(BoardDto boardDto){
         boardService.savePost(boardDto);
         return "redirect:/";
     }
 
+    @Operation(summary = "test", description = "delete api example")
     @DeleteMapping("/post/{no}") //삭제
     public String delete(@PathVariable("no") Long id){
         boardService.deletePost(id);
 
         return "redirect:/";
     }
+    @Operation(summary = "test", description = "search api example")
     @GetMapping("/board/search") // 검색s
     public String search(@RequestParam(value = "keyword") String keyword, Model model){
         List<BoardDto> boardDtoList = boardService.searchPosts(keyword);
