b1a0eba5011b1ac600679edfb4cb597a0de12fb5
jeongbeomgyu <matis12@naver.com>
2023-07-13 15:10:48 +0900

    Board with Swagger

---
+++ JeongBeomGyu/Board/Board/src/main/java/board/Board/controller/BoardController.java
@@ -0,0 +1,38 @@
+package board.Board.controller;
+
+import board.Board.dto.BoardDto;
+import board.Board.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class BoardController {
+
+    private BoardService boardService;
+
+    private BoardController(BoardService boardService){
+        this.boardService = boardService;
+    }
+
+    @GetMapping("/")
+    public String list(Model model){
+        List<BoardDto> boardDtoList = boardService.getBoardList();
+        model.addAttribute("boardList", boardDtoList);
+        return "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String write(){
+        return "board/write.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto){
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+}
